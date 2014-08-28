06a65b61b8dccbe1bfc610700e1a26490a9ff452
Timm Letzel <timm@hojoki.com>
2014-08-28 15:24:02 +0200

    First try of starting Mongo with chef

---
+++ cookbooks/ma-mongo/metadata.rb
@@ -0,0 +1,7 @@
+name             'ma-mongo'
+maintainer       'HOJOKI'
+maintainer_email 'timm@hojoki'
+license          'All rights reserved'
+description      'Installs/Configures mongo Container'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
